7ff68d2edc6b25b1f3a76ac65d024319958cb3b4
alexblch <66559372+alexblch@users.noreply.github.com>
2023-04-06 21:10:20 +0200

    Add files via upload

--- Multilayer.hpp
+++ Multilayer.hpp
@@ -7,19 +7,18 @@ class Multilayer
 private:
     float *data; //tableau de données
     float *weight;//tableau de poids
-    float *bias; //tableau de biais
+    float bias; //tableau de biais
     float *output; //tableau de sortie
     int data_size;
     int weight_size;
-    int bias_size;
     int output_size;
     int *hidden_Layer; //tableau de couches cachées
     int hidden_Layer_size;
 public:
-    Multilayer(int data_size, int weight_size, int bias_size, int output_size);
+    Multilayer(int data_size, int weight_size, int bias, int output_size);
     float sigmoid(float x);
-    float *setBias (float n, float *bias, int bias_size);
-    float activation(float *data, float *weight, float *bias, int data_size, int weight_size, int bias_size);
+    float activation(float *data, float *weight, float bias, int data_size, int weight_size);
+    float tanh(float x);
     ~Multilayer();
 };
 
@@ -28,32 +27,30 @@ float Multilayer::sigmoid(float x) //sigmoid function
     return 1 / (1 + exp(-x));
 }
 
-float *Multilayer::setBias (float n, float *bias, int bias_size) //
+float Multilayer::tanh(float x) //tanh function
 {
-    for (int i = 0; i < bias_size; i++)
-        bias[i] = n;
-    return bias;
+    return (exp(x) - exp(-x)) / (exp(x) + exp(-x));
 }
 
-float Multilayer::activation(float *data, float *weight, float *bias, int data_size, int weight_size, int bias_size) //développement du perceptron
+
+
+float Multilayer::activation(float *data, float *weight, float bias, int data_size, int weight_size) //développement du perceptron
 {
     float sum = 0;
     for (int i = 0; i < data_size; i++)
         sum += data[i] * weight[i];
-    for (int i = 0; i < bias_size; i++)
-        sum += bias[i];
-    return sigmoid(sum);
+    sum += bias;
+    return tanh(sum);
 }
 
-Multilayer::Multilayer(int data_size, int weight_size, int bias_size, int output_size) //constructor
+Multilayer::Multilayer(int data_size, int weight_size, int bias, int output_size) //constructor
 {
     data = new float[data_size];
     weight = new float[weight_size];
-    bias = new float[bias_size];
     output = new float[output_size];
     this->data_size = data_size;
     this->weight_size = weight_size;
-    this->bias_size = bias_size;
+    this->bias = bias;
     this->output_size = output_size;
 }
 
