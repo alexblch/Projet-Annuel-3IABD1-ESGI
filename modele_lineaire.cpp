56d3ad718f218d204ccd8e437e1762f749e6b3a8
ddesangesl <desangeslukombo@gmail.com>
2023-07-11 03:10:11 +0200

    Sauvegarde et chargement des modèle

--- modele_lineaire.cpp
+++ modele_lineaire.cpp
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <string>
 #include <utility>
+#include <fstream>
+#include <iostream>
 
 #ifdef WIN32
 #define DLLEXPORT __declspec(dllexport)
@@ -45,17 +47,15 @@ DLLEXPORT int *set_label_three_output(int fooball_img_list_size, int basket_img_
     for (int i = 0; i < fooball_img_list_size; i++) {
         label_list[i] = 1;
     }
-    for (int i = fooball_img_list_size; i < basket_img_list_size+fooball_img_list_size; i++) {
+    for (int i = fooball_img_list_size; i < basket_img_list_size + fooball_img_list_size; i++) {
         label_list[i] = 2;
     }
-    for (int i = basket_img_list_size+fooball_img_list_size; i < tennis_img_list_size+basket_img_list_size+fooball_img_list_size; i++) {
+    for (int i = basket_img_list_size + fooball_img_list_size;
+         i < tennis_img_list_size + basket_img_list_size + fooball_img_list_size; i++) {
         label_list[i] = 3;
     }
     return label_list;
 }
-
-
-
 
 
 DLLEXPORT float sigmoid(float output) {
@@ -80,20 +80,21 @@ DLLEXPORT float get_weighted_sum(const int *input, const float *weight, int img_
     return weighted_sum;
 }
 
-DLLEXPORT int get_class_three_output(int *img, float *weight_football, float *weight_basket, float *weight_tennis, int img_size) {
+DLLEXPORT int
+get_class_three_output(int *img, float *weight_football, float *weight_basket, float *weight_tennis, int img_size) {
     auto *output = new float[3];
-    output[0] = sigmoid(get_weighted_sum(img,weight_football,img_size));
-    output[1] = sigmoid(get_weighted_sum(img,weight_basket,img_size));
-    output[2] = sigmoid(get_weighted_sum(img,weight_tennis,img_size));
+    output[0] = sigmoid(get_weighted_sum(img, weight_football, img_size));
+    output[1] = sigmoid(get_weighted_sum(img, weight_basket, img_size));
+    output[2] = sigmoid(get_weighted_sum(img, weight_tennis, img_size));
 
     int index_biggest_output = 0;
-    for(int i = 1; i<3; i++){
-        if(output[i]>output[index_biggest_output]){
+    for (int i = 1; i < 3; i++) {
+        if (output[i] > output[index_biggest_output]) {
             index_biggest_output = i;
         }
     }
     delete[] output;
-    return index_biggest_output+1;
+    return index_biggest_output + 1;
 }
 
 DLLEXPORT float *initialize_weight(int size) {
@@ -104,11 +105,6 @@ DLLEXPORT float *initialize_weight(int size) {
     return weight_list;
 }
 
-DLLEXPORT void add_bias(int **img_list, int img_list_size, int img_size) {
-    for(int i =0; i<img_list_size;i++){
-        img_list[i][img_size] = 1;
-    }
-}
 
 DLLEXPORT int get_output_and_set_weight(int *input, float *weight, int weight_list_size, int label) {
     float weighted_sum = get_weighted_sum(input, weight, weight_list_size);
@@ -119,13 +115,84 @@ DLLEXPORT int get_output_and_set_weight(int *input, float *weight, int weight_li
     return output;
 }
 
+DLLEXPORT void save_modele(float *weight_list, int list_size, std::string path) {
+    std::ofstream outputFile(path);
+    if (!outputFile) {
+        std::cerr << "Erreur : impossible d'ouvrir le fichier " << path << std::endl;
+        return;
+    }
+    for (int i = 0; i < list_size; i++) {
+        outputFile << weight_list[i] << std::endl;
+    }
+    outputFile.close();
+    std::cout << "File written successfully." << std::endl;
+}
+
+DLLEXPORT float *load_model(int nb_weight, const std::string &path) {
+    int nb = 54;
+    auto *weight_list = new float[nb_weight];
+    std::ifstream inputFile(path);
+    if (!inputFile) {
+        std::cerr << "Erreur : impossible d'ouvrir le fichier " << path << std::endl;
+        return nullptr;
+    }
+
+    for (int i = 0; i < nb_weight; i++) {
+        if (!(inputFile >> weight_list[i])) {
+            std::cerr << "Erreur de lecture du fichier." << std::endl;
+            delete[] weight_list;
+            return nullptr;
+        }
+    }
+
+    for (int i = 0; i < nb_weight; i++) {
+        std::cout << weight_list[i] << std::endl;
+    }
+    return weight_list;
+
+}
+
+DLLEXPORT void add_bias(int **img_list, int img_list_size, int img_size) {
+    for (int i = 0; i < img_list_size; i++) {
+        img_list[i][img_size] = 1;
+    }
+}
+
+DLLEXPORT int **
+test_linear_model(int **img_list, const int *nb_image_per_class, int img_size, const char *football_model_path,
+                  const char *basket_model_path, const char *tennis_model_path) {
+    //float* list = load_model(img_size+1, "C:\\Users\\desan\\Documents\\ESGI\\PA\\testLoadWeight\\weight.txt");
+    float *football_model = load_model(img_size + 1, football_model_path);
+    float *basket_model = load_model(img_size + 1, basket_model_path);
+    float *tennis_model = load_model(img_size + 1, tennis_model_path);
+    int fooball_img_list_size = nb_image_per_class[0];
+    int basket_img_list_size = nb_image_per_class[1];
+    int tennis_img_list_size = nb_image_per_class[2];
+    int img_list_size = fooball_img_list_size + basket_img_list_size + tennis_img_list_size;
+    int **label_and_output = new int *[2];
+    int *output_list = new int[img_list_size];
+    //Ajout du biais pour chaque image
+    for (int i = 0; i < img_list_size; i++) {
+        img_list[i][img_size] = 1;
+    }
+
+    for (int i = 0; i < img_list_size; i++) {
+        output_list[i] = get_class_three_output(img_list[i], football_model, basket_model, tennis_model, img_size);
+    }
+
+    label_and_output[0] = set_label_three_output(fooball_img_list_size, basket_img_list_size, tennis_img_list_size);
+    label_and_output[1] = output_list;
+    return label_and_output;
+}
+
 
 DLLEXPORT float **
-train_linear_model(int **img_list, const int *size_class, int img_size, int training_iteration, const std::string &className) {
-    int fooball_img_list_size = size_class[0];
-    int basket_img_list_size = size_class[1];
-    int tennis_img_list_size = size_class[2];
-    int img_list_size = fooball_img_list_size + basket_img_list_size + tennis_img_list_size;
+train_linear_model(int **img_list, const int *size_class, int img_size, int training_iteration,
+                   const std::string file_path, const std::string &className) {
+    int nb_fooball_img_list = size_class[0];
+    int nb_basket_img_list = size_class[1];
+    int nb_img_list_size = size_class[2];
+    int img_list_size = nb_fooball_img_list + nb_basket_img_list + nb_img_list_size;
     add_bias(img_list, img_list_size, img_size);
     auto **weight_and_output = new float *[2];
     float *weight_list = initialize_weight(img_size + 1);
@@ -137,7 +204,7 @@ train_linear_model(int **img_list, const int *size_class, int img_size, int trai
 
     for (int i = 0; i < training_iteration; i++) {
         random_img_index = getRandomValue(img_list_size - 1);
-        label = set_label_two_output(random_img_index, fooball_img_list_size, basket_img_list_size, className);
+        label = set_label_two_output(random_img_index, nb_fooball_img_list, nb_basket_img_list, className);
         output = get_output_and_set_weight(img_list[random_img_index], weight_list, img_size + 1, label);
         if (output == label) {
             output_list[i] = 1.0;
@@ -145,43 +212,29 @@ train_linear_model(int **img_list, const int *size_class, int img_size, int trai
             output_list[i] = 0.0;
         }
     }
-
+    save_modele(weight_list, img_size + 1, file_path);
     weight_and_output[0] = weight_list;
     weight_and_output[1] = output_list;
 
     return weight_and_output;
 }
 
-DLLEXPORT int **test_linear_model(int **img_list, const int *size_class, float **weight_list,int img_size){
-    int fooball_img_list_size = size_class[0];
-    int basket_img_list_size = size_class[1];
-    int tennis_img_list_size = size_class[2];
-    int img_list_size = fooball_img_list_size + basket_img_list_size + tennis_img_list_size;
-    add_bias(img_list, img_list_size, img_size);
-    int **label_and_output = new int *[2];
-    int *output_list = new int[img_list_size];
 
-    for(int i = 0; i<img_list_size; i++){
-        output_list[i] = get_class_three_output(img_list[i],weight_list[0], weight_list[1], weight_list[2], img_size);
-    }
-
-    label_and_output[0] = set_label_three_output(fooball_img_list_size, basket_img_list_size, tennis_img_list_size);
-    label_and_output[1] = output_list;
-    return label_and_output;
+DLLEXPORT float **
+train_linear_model_football(int **img_list, const int *size_class, int img_size, int training_iteration,
+                            const char *file_path) {
+    return train_linear_model(img_list, size_class, img_size, training_iteration, file_path, "football");
 }
 
-DLLEXPORT float **train_linear_model_football(int **img_list, const int *size_class, int img_size, int training_iteration) {
-    return train_linear_model(img_list, size_class, img_size,training_iteration, "football");
+DLLEXPORT float **train_linear_model_basket(int **img_list, const int *size_class, int img_size, int training_iteration,
+                                            const char *file_path) {
+    return train_linear_model(img_list, size_class, img_size, training_iteration, file_path, "basket");
 }
 
-DLLEXPORT float **train_linear_model_basket(int **img_list, const int *size_class, int img_size, int training_iteration) {
-    return train_linear_model(img_list, size_class, img_size, training_iteration, "basket");
+DLLEXPORT float **train_linear_model_tennis(int **img_list, const int *size_class, int img_size, int training_iteration,
+                                            const char *file_path) {
+    return train_linear_model(img_list, size_class, img_size, training_iteration, file_path, "tennis");
 }
-
-DLLEXPORT float **train_linear_model_tennis(int **img_list, const int *size_class, int img_size, int training_iteration) {
-    return train_linear_model(img_list, size_class, img_size, training_iteration, "tennis");
-}
-
 
 
 }
--- modele_lineaire.py
+++ modele_lineaire.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
         return img_flatten_list
 
 
-    def train_linear_model(img_list, class_size, img_size, className, training_iteration, my_lib):
+    def train_linear_model(img_list, class_size, img_size, className, training_iteration, my_lib, modele_file_path):
         if className == "football":
             func = my_lib.train_linear_model_football
         elif className == "basket":
@@ -58,13 +58,13 @@ if __name__ == "__main__":
         ctype_class_size = class_size.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
         func.argtypes = [ctypes.POINTER(ctypes.POINTER(ctypes.c_int)),
                          ctypes.POINTER(ctypes.c_int),
-                         ctypes.c_int, ctypes.c_int]
+                         ctypes.c_int, ctypes.c_int, ctypes.c_char_p]
         func.restype = ctypes.POINTER(ctypes.POINTER(ctypes.c_float))
-        list_train = func(ctype_img_list, ctype_class_size, img_size, training_iteration)
+        list_train = func(ctype_img_list, ctype_class_size, img_size, training_iteration, modele_file_path.encode())
         return list_train
 
 
-    def test_linear_model(img_list, class_size, img_size, weight_list, my_lib):
+    def test_linear_model(img_list, class_size, img_size, path_file_model_football, path_file_model_basket, path_file_model_tennis, my_lib):
         func = my_lib.test_linear_model
         ctype_img_list = (ctypes.POINTER(ctypes.c_int) * len(img_list))()
         for i in range(len(img_list)):
@@ -72,18 +72,12 @@ if __name__ == "__main__":
             sublist_arr = (ctypes.c_int * (len(sublist) + 1))(*sublist)
             ctype_img_list[i] = sublist_arr
 
-        ctype_weight_list = (ctypes.POINTER(ctypes.c_float) * len(weight_list))()
-        for i in range(len(weight_list)):
-            sublist = weight_list[i]
-            sublist_arr = sublist
-            ctype_weight_list[i] = sublist_arr
-
         ctype_class_size = class_size.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
         func.argtypes = [ctypes.POINTER(ctypes.POINTER(ctypes.c_int)),
-                         ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.POINTER(ctypes.c_float)),
-                         ctypes.c_int]
+                         ctypes.POINTER(ctypes.c_int),
+                         ctypes.c_int, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p]
         func.restype = ctypes.POINTER(ctypes.POINTER(ctypes.c_int))
-        list_train = func(ctype_img_list, ctype_class_size, ctype_weight_list, img_size)
+        list_train = func(ctype_img_list, ctype_class_size, img_size, path_file_model_football.encode(), path_file_model_basket.encode(), path_file_model_tennis.encode())
         # list_train = [[4, 5, 6], [7, 8, 9]]
         return list_train
 
@@ -96,23 +90,28 @@ if __name__ == "__main__":
     basket_list_train = get_flatten_img_list(r"dataset\terrain_de_basket\train", largeur_img, hauteur_img)
     tennis_list_train = get_flatten_img_list(r"dataset\terrain_de_tennis\train", largeur_img, hauteur_img)
 
+    #IMPORTANT Mettre les chemins absolu des fichiers
+    path_file_model_football = "football_model.txt"
+    path_file_model_basket = "basket_model.txt"
+    path_file_model_tennis = "tennis_model.txt"
+
     img_list_train = foot_list_train + basket_list_train + tennis_list_train
 
     class_size_train = np.array([len(foot_list_train), len(basket_list_train), len(tennis_list_train)])
 
     result_train_football_train = train_linear_model(img_list_train, class_size_train, largeur_img * hauteur_img,
-                                                     "football",training_iteration, my_lib)
+                                                     "football", training_iteration, my_lib, path_file_model_football)
     result_train_basket_train = train_linear_model(img_list_train, class_size_train, largeur_img * hauteur_img,
-                                                   "basket", training_iteration,my_lib)
+                                                   "basket", training_iteration, my_lib, path_file_model_basket)
     result_train_tennis_train = train_linear_model(img_list_train, class_size_train, largeur_img * hauteur_img,
-                                                   "tennis",training_iteration, my_lib)
+                                                   "tennis", training_iteration, my_lib, path_file_model_tennis)
     img_list_size_train = len(foot_list_train) + len(basket_list_train) + len(tennis_list_train)
     train_football = []
     success = 0
     for i in range(training_iteration):
         if result_train_football_train[1][i] == 1.0:
             success += 1
-        train_football.append(success / (i+1) * 100)
+        train_football.append(success / (i + 1) * 100)
     print("Traning Football Linear Modele Accuracy :", "%.2f" % (success / training_iteration * 100), "%")
     plt.plot(train_football)
     plt.xlabel('Itération')
@@ -156,7 +155,7 @@ if __name__ == "__main__":
 
     class_size_test = np.array([len(foot_list_test), len(basket_list_test), len(tennis_list_test)])
     ctype_class_size_test = class_size_test.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
-    label_and_output = test_linear_model(img_list_test, class_size_test, largeur_img * largeur_img, weight_list_train,
+    label_and_output = test_linear_model(img_list_test, class_size_test, largeur_img * largeur_img, path_file_model_football, path_file_model_basket, path_file_model_tennis,
                                          my_lib)
     img_list_size_test = len(foot_list_test) + len(basket_list_test) + len(tennis_list_test)
     success = 0
@@ -164,13 +163,12 @@ if __name__ == "__main__":
     for i in range(img_list_size_test):
         if label_and_output[0][i] == label_and_output[1][i]:
             success += 1
-        test_result.append(success / (i+1) * 100)
+        test_result.append(success / (i + 1) * 100)
     plt.plot(test_result)
     plt.xlabel('Itération')
     plt.ylabel('Accuracy(%)')
     plt.title('Test Linear Model')
-    plt.ylim(1,100)
+    plt.ylim(1, 100)
     plt.show()
     print("Test Modele Accuracy :", "%.2f" % (success / img_list_size_test * 100), "%")
-    print("Temps d'éxécution :", round(time.time() - start, 2),"secondes")
-
+    print("Temps d'éxécution :", round(time.time() - start, 2), "secondes")
